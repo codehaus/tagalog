6a9fd66724ade746274383d829d1bf2a026782b3
mhw <mhw@ea12b545-ab72-0410-9408-e459790c8658>
2004-01-27 22:02:17 +0000

    Refactor Proc to contain a separate sequence object. There's no syntax to create nested sequences though - we don't need it at the moment.

--- trunk/tagalog/tagalog-jdbc/src/main/java/org/codehaus/tagalog/jdbc/Proc.java
+++ trunk/tagalog/tagalog-jdbc/src/main/java/org/codehaus/tagalog/jdbc/Proc.java
@@ -1,5 +1,5 @@
 /*
- * $Id: Proc.java,v 1.2 2004-01-23 18:49:24 mhw Exp $
+ * $Id: Proc.java,v 1.3 2004-01-27 22:02:17 mhw Exp $
  *
  * Copyright (c) 2003 Fintricity Limited. All Rights Reserved.
  *
@@ -10,26 +10,19 @@
 
 package com.fintricity.jdbc;
 
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
-
 /**
- * Representation of a procedure, a list of statements.
+ * Representation of a procedure, which attaches a name and a connection
+ * to a {@link ProcStatement}.
  *
  * @author Mark H. Wilkinson
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
 public final class Proc {
     private String name;
 
     private String connectionName;
 
-    private List statementList = new java.util.ArrayList();
-
-    private ProcStatement[] statements;
-
-    private Set argumentNames = new java.util.HashSet();
+    private ProcStatement body;
 
     public void setName(String name) {
         this.name = name;
@@ -47,41 +40,8 @@ public final class Proc {
         return connectionName;
     }
 
-    public void addArgumentName(String argumentName) {
-        argumentNames.add(argumentName);
-    }
-
-    public void addStatement(ProcStatement statement) {
-        if (statementList == null)
-            throw new IllegalStateException("addStatement called after"
-                                            +" statement list completed");
-        statementList.add(statement);
-    }
-
-    public void closeStatementList() {
-        statements = (ProcStatement[]) statementList.toArray(
-                                                    ProcStatement.EMPTY_ARRAY);
-    }
-
-    public String toString() {
-        StringBuffer buf = new StringBuffer();
-        buf.append(name);
-        buf.append(" { ");
-        if (statementList != null) {
-            Iterator i = statementList.iterator();
-            while (i.hasNext()) {
-                ProcStatement stmt = (ProcStatement) i.next();
-                buf.append(stmt);
-                buf.append("; ");
-            }
-        } else if (statements != null) {
-            for (int i = 0; i < statements.length; i++) {
-                buf.append(statements[i]);
-                buf.append("; ");
-            }
-        }
-        buf.append('}');
-        return buf.toString();
+    public void setBody(ProcStatement body) {
+        this.body = body;
     }
 
     public Object execute(Catalog catalog, ProcContext ctx)
@@ -92,15 +52,7 @@ public final class Proc {
         ctx.setConnectionName(connectionName);
         try {
             ctx.begin();
-            for (int i = 0; i < statements.length; i++) {
-                Object o = statements[i].execute(catalog, this, ctx);
-                if (o != null) {
-                    if (result instanceof DiscardableProcResult) {
-                        ((DiscardableProcResult) result).discard();
-                    }
-                    result = o;
-                }
-            }
+            result = body.execute(catalog, this, ctx);
         } finally {
             ctx.end();
         }
