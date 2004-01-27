a64e9680adf50d20f4c7cef77d0d1fb131575e61
mhw <mhw@ea12b545-ab72-0410-9408-e459790c8658>
2004-01-27 22:00:15 +0000

    Allow the statement that caused the error to be included in the error message.

--- trunk/tagalog/tagalog-jdbc/src/main/java/org/codehaus/tagalog/jdbc/ProcException.java
+++ trunk/tagalog/tagalog-jdbc/src/main/java/org/codehaus/tagalog/jdbc/ProcException.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ProcException.java,v 1.1 2004-01-23 15:21:36 mhw Exp $
+ * $Id: ProcException.java,v 1.2 2004-01-27 22:00:15 mhw Exp $
  *
  * Copyright (c) 2003 Fintricity Limited. All Rights Reserved.
  *
@@ -11,18 +11,32 @@
 package com.fintricity.jdbc;
 
 /**
- * Wrapper exception for errors during {@link Proc} execution.
+ * Wrapper exception for errors during {@link Proc} and
+ * {@link ProcStatement} execution.
  *
  * @author Mark H. Wilkinson
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class ProcException extends Exception {
+
+    private final ProcStatement errorBlock;
 
     /**
      * @param message
      */
     public ProcException(String message) {
         super(message);
+        this.errorBlock = null;
+    }
+
+    /**
+     * @param message
+     * @param errorBlock The <code>ProcStatement</code> that caused the
+     * error.
+     */
+    public ProcException(String message, ProcStatement errorBlock) {
+        super(message);
+        this.errorBlock = errorBlock;
     }
 
     /**
@@ -30,6 +44,17 @@ public class ProcException extends Exception {
      */
     public ProcException(Throwable cause) {
         super(cause);
+        this.errorBlock = null;
+    }
+
+    /**
+     * @param cause
+     * @param errorBlock The <code>ProcStatement</code> that caused the
+     * error.
+     */
+    public ProcException(Throwable cause, ProcStatement errorBlock) {
+        super(cause);
+        this.errorBlock = errorBlock;
     }
 
     /**
@@ -38,5 +63,34 @@ public class ProcException extends Exception {
      */
     public ProcException(String message, Throwable cause) {
         super(message, cause);
+        this.errorBlock = null;
+    }
+
+    /**
+     * @param message
+     * @param cause
+     * @param errorBlock The <code>ProcStatement</code> that caused the
+     * error.
+     */
+    public ProcException(String message, Throwable cause,
+                         ProcStatement errorBlock)
+    {
+        super(message, cause);
+        this.errorBlock = errorBlock;
+    }
+
+    /**
+     * @return
+     */
+    public ProcStatement getErrorBlock() {
+        return errorBlock;
+    }
+
+    public String getMessage() {
+        if (errorBlock != null) {
+            return super.getMessage() + " (in " + errorBlock.toString() + ")";
+        } else {
+            return super.getMessage();
+        }
     }
 }
