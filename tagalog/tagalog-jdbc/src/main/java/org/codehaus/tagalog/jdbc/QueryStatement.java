986c83c31e9d8cf75f29c21faf7976ec80647d89
mhw <mhw@ea12b545-ab72-0410-9408-e459790c8658>
2004-01-28 11:29:25 +0000

    Refactor ResultSetWrapper creation into QueryStatement, where it belongs.

--- tagalog/tagalog-jdbc/src/main/java/org/codehaus/tagalog/jdbc/QueryStatement.java
+++ tagalog/tagalog-jdbc/src/main/java/org/codehaus/tagalog/jdbc/QueryStatement.java
@@ -1,5 +1,5 @@
 /*
- * $Id: QueryStatement.java,v 1.2 2004-01-23 18:49:24 mhw Exp $
+ * $Id: QueryStatement.java,v 1.3 2004-01-28 11:29:25 mhw Exp $
  *
  * Copyright (c) 2003 Fintricity Limited. All Rights Reserved.
  *
@@ -10,12 +10,30 @@
 
 package com.fintricity.jdbc;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
  * @author Mark H. Wilkinson
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
 public final class QueryStatement extends SQLStatement {
     public void setQueryType(QueryType queryType) {
         super.setQueryType(queryType);
+    }
+
+    public Object execute(Catalog catalog, Proc proc, ProcContext ctx)
+        throws ProcException
+    {
+        PreparedStatement stmt = super.prepareAndExecute(catalog, proc, ctx);
+
+        if (stmt == null) {
+            throw new ProcException("query created no result set", this);
+        }
+        try {
+            return new ResultSetWrapper(getQueryType(), ctx, stmt);
+        } catch (SQLException e) {
+            throw new ProcException(e, this);
+        }
     }
 }
