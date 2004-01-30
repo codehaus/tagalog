2e7ddb84e69623695f74ea08983212bee9e804e1
mhw <mhw@ea12b545-ab72-0410-9408-e459790c8658>
2004-01-30 12:11:47 +0000

    Add tests for QueryType; make fromString throw an exception on an invalid parameter.

---
+++ trunk/tagalog/tagalog-jdbc/src/test/java/org/codehaus/tagalog/jdbc/QueryTypeTest.java
@@ -0,0 +1,60 @@
+/*
+ * $Id: QueryTypeTest.java,v 1.1 2004-01-30 12:11:47 mhw Exp $
+ *
+ * Copyright (c) 2003 Fintricity Limited. All Rights Reserved.
+ *
+ * THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF FINTRICITY LIMITED
+ * The copyright notice above does not evidence any actual or
+ * intended publication of such source code.
+ */
+
+package com.fintricity.jdbc;
+
+import junit.framework.TestCase;
+
+/**
+ * Simple tests for the {@link QueryType} class.
+ *
+ * @author Mark H. Wilkinson
+ * @version $Revision: 1.1 $
+ */
+public class QueryTypeTest extends TestCase {
+
+    private static final String[] types = {
+        "zero", "zero-or-one", "one", "zero-or-more", "one-or-more"
+    };
+
+    public void testToString() {
+        QueryType qt;
+
+        for (int i = 0; i < types.length; i++) {
+            qt = QueryType.fromString(types[i]);
+            assertEquals(types[i], qt.toString());
+        }
+    }
+
+    public void testGetMinimumRowCount() {
+        assertEquals(0, QueryType.fromString("zero").getMinimumRowCount());
+        assertEquals(0, QueryType.fromString("zero-or-one").getMinimumRowCount());
+        assertEquals(1, QueryType.fromString("one").getMinimumRowCount());
+        assertEquals(0, QueryType.fromString("zero-or-more").getMinimumRowCount());
+        assertEquals(1, QueryType.fromString("one-or-more").getMinimumRowCount());
+    }
+
+    public void testFromString() {
+        try {
+            QueryType qt = QueryType.fromString(null);
+            fail("fromString should throw exception");
+        } catch (IllegalArgumentException e) {
+            // expected
+        }
+
+        try {
+            QueryType qt = QueryType.fromString("bogus");
+            fail("fromString should throw exception");
+        } catch (IllegalArgumentException e) {
+            // expected
+        }
+    }
+
+}
