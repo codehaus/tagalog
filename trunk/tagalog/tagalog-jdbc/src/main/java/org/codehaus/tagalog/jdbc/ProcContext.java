8adafd391e2626ca4936583138a66e8a7fe388f6
mhw <mhw@ea12b545-ab72-0410-9408-e459790c8658>
2004-01-30 12:14:52 +0000

    Push attributes into a TreeMap so we can pull them back out in alphabetical order.

--- trunk/tagalog/tagalog-jdbc/src/main/java/org/codehaus/tagalog/jdbc/ProcContext.java
+++ trunk/tagalog/tagalog-jdbc/src/main/java/org/codehaus/tagalog/jdbc/ProcContext.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ProcContext.java,v 1.3 2004-01-23 18:49:24 mhw Exp $
+ * $Id: ProcContext.java,v 1.4 2004-01-30 12:14:52 mhw Exp $
  *
  * Copyright (c) 2003 Fintricity Limited. All Rights Reserved.
  *
@@ -10,12 +10,24 @@
 
 package com.fintricity.jdbc;
 
+import java.io.InputStream;
+import java.io.Reader;
+import java.math.BigDecimal;
+import java.net.URL;
+import java.sql.Array;
+import java.sql.Blob;
+import java.sql.Clob;
 import java.sql.Connection;
+import java.sql.Date;
+import java.sql.Ref;
 import java.sql.SQLException;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.Calendar;
 import java.util.Collections;
 import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.Set;
+import java.util.SortedMap;
 
 import org.codehaus.plexus.PlexusContainer;
 import org.codehaus.plexus.component.repository.exception.ComponentLookupException;
@@ -24,35 +36,13 @@ import org.codehaus.plexus.component.repository.exception.ComponentLookupExcepti
  * The context within which a procedure will be executed.
  *
  * @author Mark H. Wilkinson
- * @version $Revision: 1.3 $
+ * @version $Revision: 1.4 $
  */
 public final class ProcContext {
-    /**
-     * Name-value pair in the context.
-     */
-    public static final class NameValue {
-        String name;
-        String value;
 
-        NameValue(String name, String value) {
-            this.name = name;
-            this.value = value;
-        }
-    }
-
-    private List attributes = new LinkedList();
-
-    public void setAttribute(String name, String value) {
-        if (name == null)
-            throw new NullPointerException("name is null");
-        attributes.add(new NameValue(name, value));
-    }
-
-    public Iterator attributeIterator() {
-        return Collections.unmodifiableList(attributes).iterator();
-    }
-
+    //
     // Connection management.
+    //
 
     private String connectionName;
 
@@ -147,5 +137,226 @@ public final class ProcContext {
                 connection = null;
             }
         }
+    }
+
+    //
+    // Attribute management
+    //
+
+    private SortedMap attributes = new java.util.TreeMap();
+
+    Iterator attributeIterator() {
+        Set entries = Collections.unmodifiableSortedMap(attributes).entrySet();
+        return entries.iterator();
+    }
+    
+    public void setObject(String name, Object value) {
+        if (name == null)
+            throw new NullPointerException("name is null");
+        attributes.put(name, value);
+    }
+
+    //
+    // Substitutes for equivalent methods on {@link PreparedStatement}.
+    //
+    
+    /* (non-Javadoc)
+     * @see java.sql.PreparedStatement#setArray(int, java.sql.Array)
+     */
+    public void setArray(String name, Array x) {
+        throw new UnsupportedOperationException();
+    }
+    
+    /* (non-Javadoc)
+     * @see java.sql.PreparedStatement#setAsciiStream(int, java.io.InputStream, int)
+     */
+    public void setAsciiStream(String name, InputStream x, int length) {
+        throw new UnsupportedOperationException();
+    }
+    
+    /* (non-Javadoc)
+     * @see java.sql.PreparedStatement#setBigDecimal(int, java.math.BigDecimal)
+     */
+    public void setBigDecimal(String name, BigDecimal x) {
+        throw new UnsupportedOperationException();
+    }
+    
+    /* (non-Javadoc)
+     * @see java.sql.PreparedStatement#setBinaryStream(int, java.io.InputStream, int)
+     */
+    public void setBinaryStream(String name, InputStream x, int length) {
+        throw new UnsupportedOperationException();
+    }
+    
+    /* (non-Javadoc)
+     * @see java.sql.PreparedStatement#setBlob(int, java.sql.Blob)
+     */
+    public void setBlob(String name, Blob x) {
+        throw new UnsupportedOperationException();
+    }
+    
+    /* (non-Javadoc)
+     * @see java.sql.PreparedStatement#setBoolean(int, boolean)
+     */
+    public void setBoolean(String name, boolean x) {
+        throw new UnsupportedOperationException();
+    }
+    
+    /* (non-Javadoc)
+     * @see java.sql.PreparedStatement#setByte(int, byte)
+     */
+    public void setByte(String name, byte x) {
+        throw new UnsupportedOperationException();
+    }
+    
+    /* (non-Javadoc)
+     * @see java.sql.PreparedStatement#setBytes(int, byte[])
+     */
+    public void setBytes(String name, byte[] x) {
+        throw new UnsupportedOperationException();
+    }
+    
+    /* (non-Javadoc)
+     * @see java.sql.PreparedStatement#setCharacterStream(int, java.io.Reader, int)
+     */
+    public void setCharacterStream(String name, Reader reader, int length) {
+        throw new UnsupportedOperationException();
+    }
+    
+    /* (non-Javadoc)
+     * @see java.sql.PreparedStatement#setClob(int, java.sql.Clob)
+     */
+    public void setClob(String name, Clob x) {
+        throw new UnsupportedOperationException();
+    }
+    
+    /* (non-Javadoc)
+     * @see java.sql.PreparedStatement#setDate(int, java.sql.Date, java.util.Calendar)
+     */
+    public void setDate(String name, Date x, Calendar cal) {
+        throw new UnsupportedOperationException();
+    }
+    
+    /* (non-Javadoc)
+     * @see java.sql.PreparedStatement#setDate(int, java.sql.Date)
+     */
+    public void setDate(String name, Date x) {
+        throw new UnsupportedOperationException();
+    }
+    
+    /* (non-Javadoc)
+     * @see java.sql.PreparedStatement#setDouble(int, double)
+     */
+    public void setDouble(String name, double x) {
+        throw new UnsupportedOperationException();
+    }
+    
+    /* (non-Javadoc)
+     * @see java.sql.PreparedStatement#setFloat(int, float)
+     */
+    public void setFloat(String name, float x) {
+        throw new UnsupportedOperationException();
+    }
+    
+    /* (non-Javadoc)
+     * @see java.sql.PreparedStatement#setInt(int, int)
+     */
+    public void setInt(String name, int value) {
+        setObject(name, new Integer(value));
+    }
+    
+    /* (non-Javadoc)
+     * @see java.sql.PreparedStatement#setLong(int, long)
+     */
+    public void setLong(String name, long x) {
+        throw new UnsupportedOperationException();
+    }
+    
+    /* (non-Javadoc)
+     * @see java.sql.PreparedStatement#setNull(int, int, java.lang.String)
+     */
+    public void setNull(int paramIndex, int sqlType, String typeName) {
+        throw new UnsupportedOperationException();
+    }
+    
+    /* (non-Javadoc)
+     * @see java.sql.PreparedStatement#setNull(int, int)
+     */
+    public void setNull(String name, int sqlType) {
+        throw new UnsupportedOperationException();
+    }
+    
+    /* (non-Javadoc)
+     * @see java.sql.PreparedStatement#setObject(int, java.lang.Object, int, int)
+     */
+    public void setObject(String name, Object x, int targetSqlType, int scale) {
+        throw new UnsupportedOperationException();
+    }
+    
+    /* (non-Javadoc)
+     * @see java.sql.PreparedStatement#setObject(int, java.lang.Object, int)
+     */
+    public void setObject(String name, Object x, int targetSqlType) {
+        throw new UnsupportedOperationException();
+    }
+    
+    /* (non-Javadoc)
+     * @see java.sql.PreparedStatement#setRef(int, java.sql.Ref)
+     */
+    public void setRef(String name, Ref x) {
+        throw new UnsupportedOperationException();
+    }
+    
+    /* (non-Javadoc)
+     * @see java.sql.PreparedStatement#setShort(int, short)
+     */
+    public void setShort(String name, short x) {
+        throw new UnsupportedOperationException();
+    }
+    
+    public void setString(String name, String value) {
+        setObject(name, value);
+    }
+    
+    /* (non-Javadoc)
+     * @see java.sql.PreparedStatement#setTime(int, java.sql.Time, java.util.Calendar)
+     */
+    public void setTime(String name, Time x, Calendar cal) {
+        throw new UnsupportedOperationException();
+    }
+    
+    /* (non-Javadoc)
+     * @see java.sql.PreparedStatement#setTime(int, java.sql.Time)
+     */
+    public void setTime(String name, Time x) {
+        throw new UnsupportedOperationException();
+    }
+    
+    /* (non-Javadoc)
+     * @see java.sql.PreparedStatement#setTimestamp(int, java.sql.Timestamp, java.util.Calendar)
+     */
+    public void setTimestamp(String name, Timestamp x, Calendar cal) {
+        throw new UnsupportedOperationException();
+    }
+    
+    /* (non-Javadoc)
+     * @see java.sql.PreparedStatement#setTimestamp(int, java.sql.Timestamp)
+     */
+    public void setTimestamp(String name, Timestamp x) {
+        throw new UnsupportedOperationException();
+    }
+    
+    /* (non-Javadoc)
+     * @see java.sql.PreparedStatement#setUnicodeStream(int, java.io.InputStream, int)
+     */
+    public void setUnicodeStream(String name, InputStream x, int length) {
+        throw new UnsupportedOperationException();
+    }
+    
+    /* (non-Javadoc)
+     * @see java.sql.PreparedStatement#setURL(int, java.net.URL)
+     */
+    public void setURL(String name, URL x) {
+        throw new UnsupportedOperationException();
     }
 }
