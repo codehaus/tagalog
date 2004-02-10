6d1f8322e55c575cac99b397436c2e17eabf31f9
mhw <mhw@ea12b545-ab72-0410-9408-e459790c8658>
2004-02-10 23:52:01 +0000

    Flesh out the unit tests a bit more, to the point where they should run (if I haven't missed something).

--- trunk/tagalog/tagalog-acceptance/src/main/java/org/codehaus/tagalog/acceptance/people/PeopleTagLibrary.java
+++ trunk/tagalog/tagalog-acceptance/src/main/java/org/codehaus/tagalog/acceptance/people/PeopleTagLibrary.java
@@ -1,5 +1,5 @@
 /*
- * $Id: PeopleTagLibrary.java,v 1.1 2004-02-10 18:56:05 mhw Exp $
+ * $Id: PeopleTagLibrary.java,v 1.2 2004-02-10 23:52:01 mhw Exp $
  */
 
 package org.codehaus.tagalog.acceptance.people;
@@ -10,12 +10,13 @@ import org.codehaus.tagalog.AbstractTagLibrary;
  * A tag library containing some simple information about people.
  *
  * @author <a href="mailto:mhw@kremvax.net">Mark Wilkinson</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public final class PeopleTagLibrary extends AbstractTagLibrary {
     public static final String NS_URI = "tagalog:people";
 
     public PeopleTagLibrary() {
+        registerTag("people", PeopleTag.class);
         registerTag("person", PersonTag.class);
         registerTag("first-name", AttributeTag.class);
         registerTag("last-name", AttributeTag.class);
---
+++ trunk/tagalog/tagalog-acceptance/src/main/java/org/codehaus/tagalog/acceptance/people/Person.java
@@ -0,0 +1,33 @@
+/*
+ * $Id: Person.java,v 1.1 2004-02-10 23:52:01 mhw Exp $
+ */
+
+package org.codehaus.tagalog.acceptance.people;
+
+/**
+ * Person
+ *
+ * @author <a href="mailto:mhw@kremvax.net">Mark Wilkinson</a>
+ * @version $Revision: 1.1 $
+ */
+public final class Person {
+    private String firstName;
+
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
