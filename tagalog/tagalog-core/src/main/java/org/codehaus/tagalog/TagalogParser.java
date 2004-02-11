44c3ba54a490d041f7a98cb641759badeb83e505
mhw <mhw@ea12b545-ab72-0410-9408-e459790c8658>
2004-02-11 12:42:59 +0000

    Expand interface to allow parse context to be passed in.

--- tagalog/tagalog-core/src/main/java/org/codehaus/tagalog/TagalogParser.java
+++ tagalog/tagalog-core/src/main/java/org/codehaus/tagalog/TagalogParser.java
@@ -1,16 +1,43 @@
 /*
- * $Id: TagalogParser.java,v 1.1 2004-02-10 18:56:05 mhw Exp $
+ * $Id: TagalogParser.java,v 1.2 2004-02-11 12:42:59 mhw Exp $
  */
 
 package org.codehaus.tagalog;
 
+import java.util.Map;
+
 /**
  * <code>TagalogParser</code> defines the common interface for all Tagalog
- * parsers. This amounts to a single method to trigger the actual parse.
+ * parsers. This amounts to a couple of methods to trigger the actual parse.
  *
  * @author <a href="mailto:mhw@kremvax.net">Mark Wilkinson</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public interface TagalogParser {
-    void parse() throws Exception;
+    /**
+     * Perform the parse, using an empty <code>Map</code> to hold the
+     * parse context.
+     *
+     * @return The object returned by the {@link Tag#end()} method of the
+     * root element.
+     *
+     * @throws TagalogParseException if there is some problem parsing the
+     * content.
+     */
+    Object parse() throws TagalogParseException;
+
+    /**
+     * Perform the parse, using the supplied <code>Map</code> to hold the
+     * parse context.
+     *
+     * @param context A map used to hold shared information during the
+     * parse.
+     *
+     * @return The object returned by the {@link Tag#end()} method of the
+     * root element.
+     *
+     * @throws TagalogParseException if there is some problem parsing the
+     * content.
+     */
+    Object parse(Map context) throws TagalogParseException;
 }
