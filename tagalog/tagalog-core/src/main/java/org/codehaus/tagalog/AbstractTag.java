ed9b0b49b1f49bb8ecb506ad91fc33fa24a233a7
mhw <mhw@ea12b545-ab72-0410-9408-e459790c8658>
2004-02-11 12:42:27 +0000

    Keep up-to-date with the Tag interface. Begin adding convenience methods that mimic those provided by Jelly's TagSupport class.

--- tagalog/tagalog-core/src/main/java/org/codehaus/tagalog/AbstractTag.java
+++ tagalog/tagalog-core/src/main/java/org/codehaus/tagalog/AbstractTag.java
@@ -1,24 +1,70 @@
 /*
- * $Id: AbstractTag.java,v 1.1 2004-02-11 01:15:26 mhw Exp $
+ * $Id: AbstractTag.java,v 1.2 2004-02-11 12:42:27 mhw Exp $
  */
 
 package org.codehaus.tagalog;
+
+import java.util.Map;
 
 /**
  * AbstractTag
  *
  * @author <a href="mailto:mhw@kremvax.net">Mark Wilkinson</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public abstract class AbstractTag implements Tag {
-    public void begin() {
+    private Map context;
+
+    private Tag parent;
+
+    public void setContext(Map context) {
+        this.context = context;
     }
 
-    public Object end() {
+    protected Map getContext() {
+        return context;
+    }
+
+    public void setParent(Tag parent) {
+        this.parent = parent;
+    }
+
+    public Tag getParent() {
+        return parent;
+    }
+
+    public void begin(String elementName) {
+    }
+
+    public void text(char[] characters, int start, int length) {
+    }
+
+    public void child(Object child) {
+    }
+
+    public Object end(String elementName) throws TagalogParseException {
         return null;
     }
 
     public boolean recycle() {
         return true;
+    }
+
+    //
+    // Convenience methods to make tag implementation easier.
+    //
+
+    protected Tag findAncestorWithClass(Class tagClass) {
+        return TagUtils.findAncestorWithClass(getParent(), tagClass);
+    }
+
+    protected Tag requireAncestor(String tagName, Class tagClass)
+        throws TagalogParseException
+    {
+        Tag tag = findAncestorWithClass(tagClass);
+        if (tag == null) {
+            throw new TagalogParseException(tagName + " ancestor not found");
+        }
+        return tag;
     }
 }
