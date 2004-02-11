7bc5282a5cbeaa1b97f3263742f511fdd2444806
mhw <mhw@ea12b545-ab72-0410-9408-e459790c8658>
2004-02-11 12:46:35 +0000

    Basic implementation of the parser engine, and the SAX subclass.

--- tagalog/tagalog-core/src/main/java/org/codehaus/tagalog/AbstractParser.java
+++ tagalog/tagalog-core/src/main/java/org/codehaus/tagalog/AbstractParser.java
@@ -1,17 +1,25 @@
 /*
- * $Id: AbstractParser.java,v 1.1 2004-02-10 18:56:05 mhw Exp $
+ * $Id: AbstractParser.java,v 1.2 2004-02-11 12:46:35 mhw Exp $
  */
 
 package org.codehaus.tagalog;
+
+import java.util.Map;
 
 /**
  * AbstractParser
  *
  * @author <a href="mailto:mhw@kremvax.net">Mark Wilkinson</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public abstract class AbstractParser implements TagalogParser {
     private ParserConfiguration configuration;
+
+    private Map context;
+
+    private Object parseResult;
+
+    private Tag currentTag;
 
     /**
      * Construct an <code>AbstractParser</code> that uses the specified
@@ -21,5 +29,55 @@ public abstract class AbstractParser implements TagalogParser {
      */
     protected AbstractParser(ParserConfiguration configuration) {
         this.configuration = configuration;
+    }
+
+    public Object parse() throws TagalogParseException {
+        return parse(new java.util.HashMap());
+    }
+
+    public Object parse(Map context) throws TagalogParseException {
+        if (this.context != null)
+            throw new IllegalStateException("attempt to reuse parser");
+        this.context = context;
+        doParse();
+        return parseResult;
+    }
+
+    protected abstract void doParse() throws TagalogParseException;
+
+    protected void startElement(String namespaceUri, String elementName)
+        throws TagalogParseException
+    {
+        TagLibrary tagLibrary = configuration.findTagLibrary(namespaceUri);
+        Tag tag = tagLibrary.getTag(elementName);
+        tag.setContext(context);
+        if (currentTag != null)
+            tag.setParent(currentTag);
+        currentTag = tag;
+        tag.begin(elementName);
+    }
+
+    protected void text(char[] characters, int start, int length)
+        throws TagalogParseException
+    {
+        currentTag.text(characters, start, length);
+    }
+
+    protected void endElement(String namespaceUri, String elementName)
+        throws TagalogParseException
+    {
+        TagLibrary tagLibrary = configuration.findTagLibrary(namespaceUri);
+        Tag tag = currentTag;
+        Tag parentTag = tag.getParent();
+        Object value = tag.end(elementName);
+        tag.setContext(null);
+        if (parentTag != null)
+            tag.setParent(null);
+        currentTag = parentTag;
+        tagLibrary.releaseTag(elementName, tag);
+        if (parentTag == null)
+            parseResult = value;
+        else
+            parentTag.child(value);
     }
 }
