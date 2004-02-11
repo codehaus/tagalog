7e9dfe0ff111919d4967117604869dd5c675759c
mhw <mhw@ea12b545-ab72-0410-9408-e459790c8658>
2004-02-11 12:45:34 +0000

    Flesh out the implementation of the tags, now we've worked out what the Tag lifecycle might look like.

--- trunk/tagalog/tagalog-acceptance/src/main/java/org/codehaus/tagalog/acceptance/people/AttributeTag.java
+++ trunk/tagalog/tagalog-acceptance/src/main/java/org/codehaus/tagalog/acceptance/people/AttributeTag.java
@@ -1,17 +1,35 @@
 /*
- * $Id: AttributeTag.java,v 1.1 2004-02-10 18:56:05 mhw Exp $
+ * $Id: AttributeTag.java,v 1.2 2004-02-11 12:45:34 mhw Exp $
  */
 
 package org.codehaus.tagalog.acceptance.people;
 
-import org.codehaus.tagalog.Tag;
+import org.codehaus.tagalog.AbstractTag;
+import org.codehaus.tagalog.TagalogParseException;
 
 /**
  * AttributeTag
  *
  * @author <a href="mailto:mhw@kremvax.net">Mark Wilkinson</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
-public final class AttributeTag implements Tag {
+public final class AttributeTag extends AbstractTag {
+    private StringBuffer buffer;
 
+    public void begin(String elementName) {
+        buffer = new StringBuffer();
+    }
+
+    public void text(char[] characters, int start, int length) {
+        buffer.append(characters, start, length);
+    }
+
+    public Object end(String elementName) throws TagalogParseException {
+        PersonTag parent = (PersonTag) requireAncestor("person", PersonTag.class);
+        if (elementName.equals("first-name"))
+            parent.person.setFirstName(buffer.toString());
+        else if (elementName.equals("last-name"))
+            parent.person.setLastName(buffer.toString());
+        return null;
+    }
 }
--- trunk/tagalog/tagalog-acceptance/src/main/java/org/codehaus/tagalog/acceptance/people/PersonTag.java
+++ trunk/tagalog/tagalog-acceptance/src/main/java/org/codehaus/tagalog/acceptance/people/PersonTag.java
@@ -1,17 +1,30 @@
 /*
- * $Id: PersonTag.java,v 1.1 2004-02-10 18:56:05 mhw Exp $
+ * $Id: PersonTag.java,v 1.2 2004-02-11 12:45:34 mhw Exp $
  */
 
 package org.codehaus.tagalog.acceptance.people;
 
-import org.codehaus.tagalog.Tag;
+import org.codehaus.tagalog.AbstractTag;
 
 /**
  * PersonTag
  *
  * @author <a href="mailto:mhw@kremvax.net">Mark Wilkinson</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
-public final class PersonTag implements Tag {
+public final class PersonTag extends AbstractTag {
+    Person person;
 
+    public void begin(String elementName) {
+        person = new Person();
+    }
+
+    public Object end(String elementName) {
+        return person;
+    }
+
+    public boolean recycle() {
+        person = null;
+        return true;
+    }
 }
