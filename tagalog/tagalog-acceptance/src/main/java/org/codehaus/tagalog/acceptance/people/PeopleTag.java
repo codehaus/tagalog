68553facf984ed1c2c29a3eb7831ef96d7d9db6e
mhw <mhw@ea12b545-ab72-0410-9408-e459790c8658>
2004-02-11 12:45:34 +0000

    Flesh out the implementation of the tags, now we've worked out what the Tag lifecycle might look like.

--- tagalog/tagalog-acceptance/src/main/java/org/codehaus/tagalog/acceptance/people/PeopleTag.java
+++ tagalog/tagalog-acceptance/src/main/java/org/codehaus/tagalog/acceptance/people/PeopleTag.java
@@ -1,17 +1,35 @@
 /*
- * $Id: PeopleTag.java,v 1.1 2004-02-10 23:52:01 mhw Exp $
+ * $Id: PeopleTag.java,v 1.2 2004-02-11 12:45:34 mhw Exp $
  */
 
 package org.codehaus.tagalog.acceptance.people;
 
-import org.codehaus.tagalog.Tag;
+import org.codehaus.tagalog.AbstractTag;
 
 /**
  * PeopleTag
  *
  * @author <a href="mailto:mhw@kremvax.net">Mark Wilkinson</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
-public class PeopleTag implements Tag {
+public class PeopleTag extends AbstractTag {
+    private People people;
 
+    public void begin(String elementName) {
+        people = new People();
+    }
+
+    public void child(Object child) {
+        Person person = (Person) child;
+        people.addPerson(person);
+    }
+
+    public Object end(String elementName) {
+        return people;
+    }
+
+    public boolean recycle() {
+        people = null;
+        return true;
+    }
 }
