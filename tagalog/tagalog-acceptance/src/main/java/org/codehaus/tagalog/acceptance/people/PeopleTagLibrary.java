7a6a39462db58da243eab9e1c1ba4d3cc081731d
mhw <mhw@ea12b545-ab72-0410-9408-e459790c8658>
2004-02-19 18:19:40 +0000

    Add a tag that has a deliberate programming error in it.

--- tagalog/tagalog-acceptance/src/main/java/org/codehaus/tagalog/acceptance/people/PeopleTagLibrary.java
+++ tagalog/tagalog-acceptance/src/main/java/org/codehaus/tagalog/acceptance/people/PeopleTagLibrary.java
@@ -1,5 +1,5 @@
 /*
- * $Id: PeopleTagLibrary.java,v 1.2 2004-02-10 23:52:01 mhw Exp $
+ * $Id: PeopleTagLibrary.java,v 1.3 2004-02-19 18:19:40 mhw Exp $
  */
 
 package org.codehaus.tagalog.acceptance.people;
@@ -10,7 +10,7 @@ import org.codehaus.tagalog.AbstractTagLibrary;
  * A tag library containing some simple information about people.
  *
  * @author <a href="mailto:mhw@kremvax.net">Mark Wilkinson</a>
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
 public final class PeopleTagLibrary extends AbstractTagLibrary {
     public static final String NS_URI = "tagalog:people";
@@ -20,5 +20,6 @@ public final class PeopleTagLibrary extends AbstractTagLibrary {
         registerTag("person", PersonTag.class);
         registerTag("first-name", AttributeTag.class);
         registerTag("last-name", AttributeTag.class);
+        registerTag("broken", BrokenTag.class);
     }
 }
