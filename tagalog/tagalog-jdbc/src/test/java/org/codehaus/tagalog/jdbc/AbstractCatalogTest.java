dab8544fbf9e8e927ecdddcdcbc868a592382b3b
mhw <mhw@ea12b545-ab72-0410-9408-e459790c8658>
2004-02-25 20:40:29 +0000

    Factor out the common stuff for actually running the catalog tests.

---
+++ tagalog/tagalog-jdbc/src/test/java/org/codehaus/tagalog/jdbc/AbstractCatalogTest.java
@@ -0,0 +1,49 @@
+/*
+ * $Id: AbstractCatalogTest.java,v 1.1 2004-02-25 20:40:29 mhw Exp $
+ *
+ * Copyright (c) 2004 Fintricity Limited. All Rights Reserved.
+ *
+ * THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF FINTRICITY LIMITED
+ * The copyright notice above does not evidence any actual or
+ * intended publication of such source code.
+ */
+
+package com.fintricity.jdbc;
+
+import org.codehaus.plexus.PlexusTestCase;
+
+/**
+ * Abstract base class for the catalog tests.
+ *
+ * @author Mark H. Wilkinson
+ * @version $Revision: 1.1 $
+ */
+public abstract class AbstractCatalogTest extends PlexusTestCase {
+
+    public AbstractCatalogTest(String testName) {
+        super(testName);
+    }
+
+    protected CatalogTestGeneric genericTest;
+
+    public void setUp() throws Exception {
+        super.setUp();
+        genericTest = new CatalogTestGeneric(getContainer());
+    }
+
+    public void testQueryNoTable() throws Exception {
+        genericTest.testQueryNoTable();
+    }
+
+    public void testTableQueries() throws Exception {
+        genericTest.testTableQueries();
+    }
+
+    public void testTableQueriesWithBindVariables() throws Exception {
+        genericTest.testTableQueriesWithBindVariables();
+    }
+
+    public void testInsertWithBindVariables() throws Exception {
+        genericTest.testInsertWithBindVariables();
+    }
+}
