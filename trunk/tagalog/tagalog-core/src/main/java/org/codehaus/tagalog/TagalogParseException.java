dde7c97208830aca1cc38ed2cd0fb6678ed18ab3
mhw <mhw@ea12b545-ab72-0410-9408-e459790c8658>
2004-02-26 17:36:58 +0000

    JavaDoc

--- trunk/tagalog/tagalog-core/src/main/java/org/codehaus/tagalog/TagalogParseException.java
+++ trunk/tagalog/tagalog-core/src/main/java/org/codehaus/tagalog/TagalogParseException.java
@@ -1,14 +1,23 @@
 /*
- * $Id: TagalogParseException.java,v 1.2 2004-02-20 18:36:05 mhw Exp $
+ * $Id: TagalogParseException.java,v 1.3 2004-02-26 17:36:58 mhw Exp $
  */
 
 package org.codehaus.tagalog;
 
 /**
- * TagalogParseException
+ * <code>TagalogParseException</code> is used to wrap any exceptions
+ * thrown by the underlying XML parser. It can also be thrown by
+ * implementations of the {@link Tag} interface if they encounter a
+ * condition that makes it impossible to continue processing.
+ * However, this style of processing is discouraged: in the majority of
+ * cases it is preferrable for <code>Tag</code>s to throw
+ * {@link TagException} as this allows parsing to continue and a better
+ * error report to be prepared for the user.
+ *
+ * @see TagException
  *
  * @author <a href="mailto:mhw@kremvax.net">Mark Wilkinson</a>
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
 public class TagalogParseException extends Exception {
     public TagalogParseException() {
