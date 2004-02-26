ec57746037eb0ce037421dc6c7ef9d32cc72359e
mhw <mhw@ea12b545-ab72-0410-9408-e459790c8658>
2004-02-26 12:29:11 +0000

    Rework to operate on any statement type, so we can have dialect choices between sequences (for example).

--- trunk/tagalog/tagalog-jdbc/src/main/java/org/codehaus/tagalog/jdbc/DialectChoiceStatement.java
+++ trunk/tagalog/tagalog-jdbc/src/main/java/org/codehaus/tagalog/jdbc/DialectChoiceStatement.java
@@ -1,5 +1,5 @@
 /*
- * $Id: DialectChoiceStatement.java,v 1.2 2004-01-30 17:48:58 mhw Exp $
+ * $Id: DialectChoiceStatement.java,v 1.3 2004-02-26 12:29:11 mhw Exp $
  *
  * Copyright (c) 2004 Fintricity Limited. All Rights Reserved.
  *
@@ -16,28 +16,62 @@ import org.codehaus.plexus.component.repository.exception.ComponentLookupExcepti
 
 /**
  * Choose the appropriate matching SQL statement from a set of alternatives.
+ * The default behaviour of the dialect choice statement is to choose the
+ * contained statement whose <code>dialect</code> property matches that of
+ * the current database connection manager. If there is no match for the
+ * dialect of the connection, the contained statement whose dialect property
+ * is <code>null</code> will be executed.
+ *
+ * <p>
+ * If no dialect matches and there is no default statement this is normally
+ * considered an error: {@link ProcException} will be thrown because the
+ * dialect choice was unable to find a suitable statement.
+ * Passing the value <code>true</code> to the {@link #setOptional} method
+ * causes this condition to be silently ignored: effectively the contents
+ * of the dialect choice become optional statements to be executed only on
+ * certain types of databases.
  *
  * @author Mark H. Wilkinson
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
-public class DialectChoiceStatement extends StatementGroup
-    implements ProcStatement
-{
+public class DialectChoiceStatement implements CompoundProcStatement {
+    private boolean optional = false;
+
+    /**
+     * Indicate that this dialect choice is not mandatory; that is, it is
+     * not an error if there is no statement matching the current database
+     * dialect.
+     * 
+     * @param optional True to indicate that execution of this dialect
+     * choice is optional.
+     */
+    public void setOptional(boolean optional) {
+        this.optional = optional;
+    }
+
+    /**
+     * Return the database dialect that this {@link ProcStatement} is
+     * intended for. <code>DialectChoiceStatement</code> does not have
+     * a dialect itself, so this method always returns <code>null</code>.
+     *
+     * @return <code>null</code>
+     */
+    public String getDialect() {
+        return null;
+    }
+
+    private StatementGroup statementGroup = new StatementGroup();
+
     public void addStatement(ProcStatement statement) {
-        if (statement instanceof SQLStatement) {
-            super.addStatement(statement);
-        } else {
-            throw new IllegalArgumentException(
-                "dialect choice can only contain SQL statements"
-            );
-        }
+        statementGroup.addStatement(statement);
     }
 
     public void closeStatementList() {
-        super.closeStatementList();
+        statementGroup.closeStatementList();
+        ProcStatement[] statements = statementGroup.getStatementList();
         Set dialects = new java.util.HashSet();
         for (int i = 0; i < statements.length; i++) {
-            dialects.add(((SQLStatement) statements[i]).getDialect());
+            dialects.add(statements[i].getDialect());
         }
         if (dialects.size() != statements.length) {
             throw new IllegalStateException(
@@ -47,13 +81,14 @@ public class DialectChoiceStatement extends StatementGroup
     }
 
     public String toString() {
-        return "choice " + super.toString();
+        return "choice " + statementGroup.toString();
     }
 
     public Object execute(Catalog catalog, Proc proc, ProcContext ctx)
         throws ProcException
     {
         String requiredDialect;
+        ProcStatement[] statements = statementGroup.getStatementList();
         ProcStatement defaultStatement = null;
         ProcStatement foundStatement = null;
 
@@ -63,7 +98,7 @@ public class DialectChoiceStatement extends StatementGroup
             throw new ProcException(e);
         }
         for (int i = 0; i < statements.length; i++) {
-            SQLStatement stmt = (SQLStatement) statements[i];
+            ProcStatement stmt = statements[i];
             String statementDialect = stmt.getDialect();
             if (statementDialect == null)
                 defaultStatement = stmt;
@@ -74,6 +109,8 @@ public class DialectChoiceStatement extends StatementGroup
             return foundStatement.execute(catalog, proc, ctx);
         } else if (defaultStatement != null) {
             return defaultStatement.execute(catalog, proc, ctx);
+        } else if (optional) {
+            return null;
         } else {
             if (requiredDialect == null) {
                 throw new ProcException("no default statement", this);
