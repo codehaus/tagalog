ef307c99d64407aeafad60be9da187ed41955732
mhw <mhw@ea12b545-ab72-0410-9408-e459790c8658>
2004-02-26 17:48:59 +0000

    Validation failures are non-fatal.

--- trunk/tagalog/tagalog-acceptance/src/main/java/org/codehaus/tagalog/acceptance/people/PersonTag.java
+++ trunk/tagalog/tagalog-acceptance/src/main/java/org/codehaus/tagalog/acceptance/people/PersonTag.java
@@ -1,24 +1,24 @@
 /*
- * $Id: PersonTag.java,v 1.3 2004-02-11 17:27:54 mhw Exp $
+ * $Id: PersonTag.java,v 1.4 2004-02-26 17:48:59 mhw Exp $
  */
 
 package org.codehaus.tagalog.acceptance.people;
 
 import org.codehaus.tagalog.AbstractTag;
 import org.codehaus.tagalog.Attributes;
-import org.codehaus.tagalog.TagalogParseException;
+import org.codehaus.tagalog.TagException;
 
 /**
  * PersonTag
  *
  * @author <a href="mailto:mhw@kremvax.net">Mark Wilkinson</a>
- * @version $Revision: 1.3 $
+ * @version $Revision: 1.4 $
  */
 public final class PersonTag extends AbstractTag {
     Person person;
 
     public void begin(String elementName, Attributes attributes)
-        throws TagalogParseException
+        throws TagException
     {
         person = new Person();
         person.setUserId(requireAttribute("user-id", attributes));
