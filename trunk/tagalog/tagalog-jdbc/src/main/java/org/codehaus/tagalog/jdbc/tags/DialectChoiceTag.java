10e19a80076b1ad179ce3b96a7ff44629ec00dee
mhw <mhw@ea12b545-ab72-0410-9408-e459790c8658>
2004-02-26 12:32:26 +0000

    Rework for new model, mainly adding <sequence> and allowing dialect on any type of statement.

---
+++ trunk/tagalog/tagalog-jdbc/src/main/java/org/codehaus/tagalog/jdbc/tags/DialectChoiceTag.java
@@ -0,0 +1,33 @@
+/*
+ * $Id: DialectChoiceTag.java,v 1.1 2004-02-26 12:32:26 mhw Exp $
+ *
+ * Copyright (c) 2004 Fintricity Limited. All Rights Reserved.
+ *
+ * THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF FINTRICITY LIMITED
+ * The copyright notice above does not evidence any actual or
+ * intended publication of such source code.
+ */
+
+package com.fintricity.jdbc.tagalog;
+
+import org.codehaus.tagalog.Attributes;
+import org.codehaus.tagalog.TagalogParseException;
+
+import com.fintricity.jdbc.DialectChoiceStatement;
+
+/**
+ * @author Mark H. Wilkinson
+ * @version $Revision: 1.1 $
+ */
+public class DialectChoiceTag extends AbstractCompoundStatementTag {
+    public void begin(String elementName, Attributes attributes)
+        throws TagalogParseException
+    {
+        String s;
+
+        stmt = new DialectChoiceStatement();
+        s = attributes.getValue("optional");
+        if (s != null && s.equals("true"))
+            ((DialectChoiceStatement) stmt).setOptional(true);
+    }
+}
