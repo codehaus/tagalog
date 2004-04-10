40725f8d2c065bfe2b39b7587b73521631435cb8
mhw <mhw@ea12b545-ab72-0410-9408-e459790c8658>
2004-04-10 15:24:13 +0000

    Handle processing instructions. To do this we need to use the parser's nextToken() method, and hence also process entity references and CDATA sections separately too.

--- tagalog/tagalog-core/src/main/java/org/codehaus/tagalog/xpp/TagalogXmlPullParser.java
+++ tagalog/tagalog-core/src/main/java/org/codehaus/tagalog/xpp/TagalogXmlPullParser.java
@@ -1,5 +1,5 @@
 /*
- * $Id: TagalogXmlPullParser.java,v 1.3 2004-02-26 17:46:31 mhw Exp $
+ * $Id: TagalogXmlPullParser.java,v 1.4 2004-04-10 15:24:13 mhw Exp $
  */
 
 package org.codehaus.tagalog.xpp;
@@ -18,7 +18,7 @@ import org.codehaus.tagalog.TagalogParseException;
  * TagalogXmlPullParser
  *
  * @author <a href="mailto:mhw@kremvax.net">Mark Wilkinson</a>
- * @version $Revision: 1.3 $
+ * @version $Revision: 1.4 $
  */
 final class TagalogXmlPullParser extends AbstractParser {
     private XmlPullParser xpp;
@@ -51,21 +51,39 @@ final class TagalogXmlPullParser extends AbstractParser {
         throws TagalogParseException, XmlPullParserException, IOException
     {
         Attributes attributes = new XmlPullAttributes(xpp);
+        boolean handlingProcessingInstructions;
         int eventType;
         int[] characterOffsets = new int[2];
 
+        handlingProcessingInstructions = handlingProcessingInstructions();
         source.setInputFor(xpp);
         eventType = xpp.getEventType();
         while (eventType != XmlPullParser.END_DOCUMENT) {
             if (eventType == XmlPullParser.START_TAG) {
                 startElement(xpp.getNamespace(), xpp.getName(), attributes);
-            } else if (eventType == XmlPullParser.TEXT) {
+            } else if (eventType == XmlPullParser.TEXT
+                       || eventType == XmlPullParser.CDSECT) {
                 char[] chars = xpp.getTextCharacters(characterOffsets);
                 text(chars, characterOffsets[0], characterOffsets[1]);
             } else if (eventType == XmlPullParser.END_TAG) {
                 endElement(xpp.getNamespace(), xpp.getName());
+            } else if (eventType == XmlPullParser.ENTITY_REF) {
+                String entity = xpp.getText();
+                text(entity.toCharArray(), 0, entity.length());
+            } else if (eventType == XmlPullParser.PROCESSING_INSTRUCTION) {
+                String instruction = xpp.getText();
+                int space = instruction.indexOf(' ');
+                if (space != -1) {
+                    processingInstruction(instruction.substring(0, space),
+                                          instruction.substring(space + 1));
+                } else {
+                    processingInstruction(instruction, "");
+                }
             }
-            eventType = xpp.next();
+            if (handlingProcessingInstructions)
+                eventType = xpp.nextToken();
+            else
+                eventType = xpp.next();
         }
     }
 
