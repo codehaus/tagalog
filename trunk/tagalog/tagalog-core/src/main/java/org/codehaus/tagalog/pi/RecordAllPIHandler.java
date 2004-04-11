7e98532d5aff2fc2ef448ef687c9713275454123
mhw <mhw@ea12b545-ab72-0410-9408-e459790c8658>
2004-04-11 17:31:51 +0000

    JavaDoc and Checkstyle fixes.

--- trunk/tagalog/tagalog-core/src/main/java/org/codehaus/tagalog/pi/RecordAllPIHandler.java
+++ trunk/tagalog/tagalog-core/src/main/java/org/codehaus/tagalog/pi/RecordAllPIHandler.java
@@ -1,5 +1,5 @@
 /*
- * $Id: RecordAllPIHandler.java,v 1.1 2004-04-10 15:17:08 mhw Exp $
+ * $Id: RecordAllPIHandler.java,v 1.2 2004-04-11 17:31:51 mhw Exp $
  */
 
 package org.codehaus.tagalog.pi;
@@ -11,7 +11,7 @@ import java.util.Map;
  * A processing instruction handler that records all the data provided
  * to each processing instruction target in a document.
  * Each processing instruction target results in a context mapping from a
- * {@link PIHashKey} to a {@link List} of <code>String</code> data values.
+ * {@link PIHashKey} to a {@link List} of {@link String} data values.
  * Processing instruction data is added to end of the list,
  * so each list contains processing instruction data in the order it was
  * encountered in the document.
@@ -20,12 +20,12 @@ import java.util.Map;
  * The approach of using <code>PIHashKey</code> values to distinguish
  * processing instructions from other data in the context <code>Map</code>
  * requires that the context is held in a <code>Map</code>
- * that supports keys of different types, such as {@link HashMap}.
- * It does not work correctly with {@link TreeMap} because the keys are
- * not comparable to each other.
+ * that supports keys of different types, such as {@link java.util.HashMap}.
+ * It does not work correctly with {@link java.util.TreeMap} because the keys
+ * are not comparable to each other.
  *
  * @author <a href="mailto:mhw@kremvax.net">Mark Wilkinson</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class RecordAllPIHandler implements PIHandler {
     public static final RecordAllPIHandler INSTANCE = new RecordAllPIHandler();
