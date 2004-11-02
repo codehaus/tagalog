3f7ec9c4fc1f7a1eafa50916a52d90ece65a5b49
mhw <mhw@ea12b545-ab72-0410-9408-e459790c8658>
2004-11-02 14:36:43 +0000

    Add convenient constructor.

--- tagalog/tagalog-el/src/main/java/org/codehaus/tagalog/el/ExpressionParseException.java
+++ tagalog/tagalog-el/src/main/java/org/codehaus/tagalog/el/ExpressionParseException.java
@@ -1,17 +1,32 @@
 /*
- * $Id: ExpressionParseException.java,v 1.1 2004-10-26 19:14:34 mhw Exp $
+ * $Id: ExpressionParseException.java,v 1.2 2004-11-02 14:36:43 mhw Exp $
  */
 
 package org.codehaus.tagalog.el;
 
 /**
- * ExpressionParseException
+ * Thrown by {@link org.codehaus.tagalog.el.ExpressionParser#parse(String)} to
+ * indicate that an expression could not be parsed.
  *
  * @author <a href="mailto:mhw@kremvax.net">Mark Wilkinson</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class ExpressionParseException extends Exception {
     public ExpressionParseException(String message) {
         super(message);
+    }
+
+    public ExpressionParseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    /**
+     * Create an <code>ExpressionParseException</code> with the default
+     * message "expression could not be parsed".
+     *
+     * @param cause Exception indicating the reason for evaluation failure.
+     */
+    public ExpressionParseException(Throwable cause) {
+        super("expression could not be parsed", cause);
     }
 }
