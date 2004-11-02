84468d0f3e3828d862948fe2071b0e99e862f94b
mhw <mhw@ea12b545-ab72-0410-9408-e459790c8658>
2004-11-02 14:36:43 +0000

    Add convenient constructor.

--- trunk/tagalog/tagalog-el/src/main/java/org/codehaus/tagalog/el/ExpressionEvaluationException.java
+++ trunk/tagalog/tagalog-el/src/main/java/org/codehaus/tagalog/el/ExpressionEvaluationException.java
@@ -1,14 +1,15 @@
 /*
- * $Id: ExpressionEvaluationException.java,v 1.1 2004-10-26 19:14:34 mhw Exp $
+ * $Id: ExpressionEvaluationException.java,v 1.2 2004-11-02 14:36:43 mhw Exp $
  */
 
 package org.codehaus.tagalog.el;
 
 /**
- * ExpressionEvaluationException
+ * Thrown by {@link org.codehaus.tagalog.el.Expression#evaluate(Map)} to
+ * indicate that expression evaluation failed.
  *
  * @author <a href="mailto:mhw@kremvax.net">Mark Wilkinson</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class ExpressionEvaluationException extends Exception {
     public ExpressionEvaluationException(String message) {
@@ -17,5 +18,15 @@ public class ExpressionEvaluationException extends Exception {
 
     public ExpressionEvaluationException(String message, Throwable cause) {
         super(message, cause);
+    }
+
+    /**
+     * Create an <code>ExpressionEvaluationException</code> with the default
+     * message "expression evaluation failed".
+     *
+     * @param cause Exception indicating the reason for evaluation failure.
+     */
+    public ExpressionEvaluationException(Throwable cause) {
+        super("expression evaluation failed", cause);
     }
 }
