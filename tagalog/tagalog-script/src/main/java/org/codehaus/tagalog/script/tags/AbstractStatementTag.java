a5e087f55c1f77725106a80c0900972468b54c3d
mhw <mhw@ea12b545-ab72-0410-9408-e459790c8658>
2004-11-08 07:21:36 +0000

    Remove the CompoundStatement interface and abstract implementation - it requires implementors to be mutable when we can restrict that to the tags.
    JavaDoc.

--- tagalog/tagalog-script/src/main/java/org/codehaus/tagalog/script/tags/AbstractStatementTag.java
+++ tagalog/tagalog-script/src/main/java/org/codehaus/tagalog/script/tags/AbstractStatementTag.java
@@ -1,5 +1,5 @@
 /*
- * $Id: AbstractStatementTag.java,v 1.1 2004-11-04 18:04:57 mhw Exp $
+ * $Id: AbstractStatementTag.java,v 1.2 2004-11-08 07:21:36 mhw Exp $
  */
 
 package org.codehaus.tagalog.script.tags;
@@ -7,24 +7,50 @@ package org.codehaus.tagalog.script.tags;
 import org.codehaus.tagalog.AbstractTag;
 import org.codehaus.tagalog.Tag;
 import org.codehaus.tagalog.TagException;
-import org.codehaus.tagalog.script.CompoundStatement;
 import org.codehaus.tagalog.script.Statement;
 
 /**
+ * Shared behaviour for all tags that create {@link Statement} objects.
+ * Provides the following behaviour to classes that extend it:
+ *
+ * <ul>
+ * <li>
+ * Storage of the reference to the {@link Statement} object in the
+ * {@link #stmt} attribute. A value must be assigned to <code>stmt</code>
+ * before the {@link #end(String)} method is called.
+ * </li>
+ * <li>
+ * Adding the completed <code>stmt</code> to the nearest enclosing
+ * compound statement when the {@link #end(String)} method is called.
+ * If there is no enclosing compound statement a {@link TagException} is
+ * thrown, unless the {@link #rootTag} attribute has been set to true.
+ * </li>
+ * </ul>
+ *
+ * Subclasses will typically assign a value to the {@link #stmt} attribute
+ * in the <code>begin</code> method if all the necessary information is
+ * available from the attributes.
+ * Otherwise information can be collected during the lifecycle of the tag
+ * and the <code>Statement</code> created in the implementation of the
+ * {@link #end(String)} method, <strong>before</strong> calling
+ * <code>super.end(elementName)</code>.
+ *
  * @author Mark H. Wilkinson
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public abstract class AbstractStatementTag
     extends AbstractTag
 {
+    /**
+     * Is this tag the root tag of a script hierarchy?
+     */
     protected boolean rootTag = false;
 
     protected Statement stmt;
 
     public Object end(String elementName) throws TagException {
         Tag tag;
-        AbstractStatementTag parent;
-        CompoundStatement parentCompoundStmt;
+        AbstractCompoundStatementTag parent;
 
         if (rootTag) {
             tag = findAncestorWithClass(AbstractCompoundStatementTag.class);
@@ -34,9 +60,8 @@ public abstract class AbstractStatementTag
 
         tag = requireAncestor("compound statement",
                               AbstractCompoundStatementTag.class);
-        parent = (AbstractCompoundStatementTag) tag; 
-        parentCompoundStmt = (CompoundStatement) parent.stmt;
-        parentCompoundStmt.addStatement(stmt);
+        parent = (AbstractCompoundStatementTag) tag;
+        parent.addStatement(stmt);
         return stmt;
     }
 
