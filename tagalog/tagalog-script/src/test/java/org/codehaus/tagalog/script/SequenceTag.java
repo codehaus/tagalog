f21b1fdbf8ecc10f06359ac787870b7ebd7fd7ce
mhw <mhw@ea12b545-ab72-0410-9408-e459790c8658>
2004-11-08 07:19:03 +0000

    Add SequenceTag to the test as we've removed it from the main code base.

---
+++ tagalog/tagalog-script/src/test/java/org/codehaus/tagalog/script/SequenceTag.java
@@ -0,0 +1,14 @@
+/*
+ * $Id: SequenceTag.java,v 1.1 2004-11-08 07:19:03 mhw Exp $
+ */
+
+package org.codehaus.tagalog.script;
+
+import org.codehaus.tagalog.script.tags.AbstractCompoundStatementTag;
+
+/**
+ * @author <a href="mailto:mhw@kremvax.net">Mark Wilkinson</a>
+ * @version $Revision: 1.1 $
+ */
+public class SequenceTag extends AbstractCompoundStatementTag {
+}
--- tagalog/tagalog-script/src/test/java/org/codehaus/tagalog/script/TestScriptTagLibrary.java
+++ tagalog/tagalog-script/src/test/java/org/codehaus/tagalog/script/TestScriptTagLibrary.java
@@ -1,5 +1,5 @@
 /*
- * $Id: TestScriptTagLibrary.java,v 1.1 2004-03-02 21:45:29 mhw Exp $
+ * $Id: TestScriptTagLibrary.java,v 1.2 2004-11-08 07:19:03 mhw Exp $
  */
 
 package org.codehaus.tagalog.script;
@@ -8,7 +8,7 @@ import org.codehaus.tagalog.script.tags.ScriptTagLibrary;
 
 /**
  * @author <a href="mailto:mhw@kremvax.net">Mark Wilkinson</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class TestScriptTagLibrary extends ScriptTagLibrary {
     public static final String NS_URI
@@ -16,6 +16,7 @@ public class TestScriptTagLibrary extends ScriptTagLibrary {
 
     public TestScriptTagLibrary() {
         super();
+        registerTag("sequence", SequenceTag.class);
         registerTag("collect", CollectTag.class);
     }
 }
