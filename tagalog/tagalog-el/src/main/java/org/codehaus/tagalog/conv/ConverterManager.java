125da4b2f427f9529b7e19d5cc05fd7d4c11fc05
mhw <mhw@ea12b545-ab72-0410-9408-e459790c8658>
2005-03-01 10:32:56 +0000

    Add converters for other integer types. Convert primitive types to their wrappers.

--- tagalog/tagalog-el/src/main/java/org/codehaus/tagalog/conv/ConverterManager.java
+++ tagalog/tagalog-el/src/main/java/org/codehaus/tagalog/conv/ConverterManager.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ConverterManager.java,v 1.1 2004-12-20 19:02:49 mhw Exp $
+ * $Id: ConverterManager.java,v 1.2 2005-03-01 10:32:56 mhw Exp $
  */
 
 package org.codehaus.tagalog.conv;
@@ -7,10 +7,10 @@ package org.codehaus.tagalog.conv;
 import java.util.Map;
 
 /**
- * ConverterManager
+ * Manages a mapping from classes to {@link Converter}s.
  *
  * @author <a href="mailto:mhw@kremvax.net">Mark Wilkinson</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public final class ConverterManager {
     public static final ConverterManager INSTANCE = new ConverterManager();
@@ -19,8 +19,10 @@ public final class ConverterManager {
 
     public ConverterManager() {
         registerConverter(Boolean.class, new BooleanConverter());
-
-        registerConverter(Integer.class, new IntConverter());
+        registerConverter(Byte.class, new ByteConverter());
+        registerConverter(Short.class, new ShortConverter());
+        registerConverter(Integer.class, new IntegerConverter());
+        registerConverter(Long.class, new LongConverter());
     }
 
     public void registerConverter(Class resultClass, Converter converter) {
@@ -32,6 +34,28 @@ public final class ConverterManager {
     }
 
     public Converter getConverter(Class resultClass) {
+        if (resultClass.isPrimitive())
+            resultClass = (Class) canonicalMap.get(resultClass);
         return (Converter) converters.get(resultClass);
+    }
+
+    private static Map canonicalMap = new java.util.HashMap();
+
+    static {
+        canonicalMap.put(Void.TYPE, Void.class);
+        canonicalMap.put(Boolean.TYPE, Boolean.class);
+        canonicalMap.put(Byte.TYPE, Byte.class);
+        canonicalMap.put(Character.TYPE, Character.class);
+        canonicalMap.put(Short.TYPE, Short.class);
+        canonicalMap.put(Integer.TYPE, Integer.class);
+        canonicalMap.put(Long.TYPE, Long.class);
+        canonicalMap.put(Float.TYPE, Float.class);
+        canonicalMap.put(Double.TYPE, Double.class);
+    }
+
+    public static Class canonicalClass(Class c) {
+        if (!c.isPrimitive())
+            return c;
+        return (Class) canonicalMap.get(c);
     }
 }
