2af4b7e9c347abf4b9a5c2a26b25ed7685040593
mhw <mhw@ea12b545-ab72-0410-9408-e459790c8658>
2005-04-07 15:56:15 +0000

    Update to use TagBinding objects.

--- trunk/tagalog/tagalog-script/src/test/java/org/codehaus/tagalog/script/TestScriptTagLibrary.java
+++ trunk/tagalog/tagalog-script/src/test/java/org/codehaus/tagalog/script/TestScriptTagLibrary.java
@@ -1,22 +1,28 @@
 /*
- * $Id: TestScriptTagLibrary.java,v 1.2 2004-11-08 07:19:03 mhw Exp $
+ * $Id: TestScriptTagLibrary.java,v 1.3 2005-04-07 15:56:15 mhw Exp $
  */
 
 package org.codehaus.tagalog.script;
 
+import org.codehaus.tagalog.TagBinding;
 import org.codehaus.tagalog.script.tags.ScriptTagLibrary;
 
 /**
  * @author <a href="mailto:mhw@kremvax.net">Mark Wilkinson</a>
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
 public class TestScriptTagLibrary extends ScriptTagLibrary {
     public static final String NS_URI
         = "tagalog:test-script";
 
+    public static final TagBinding SEQUENCE = new TagBinding("sequence",
+                                                             SequenceTag.class);
+    public static final TagBinding COLLECT = new TagBinding("collect",
+                                                            CollectTag.class);
+
     public TestScriptTagLibrary() {
         super();
-        registerTag("sequence", SequenceTag.class);
-        registerTag("collect", CollectTag.class);
+        registerTagBinding(SEQUENCE);
+        registerTagBinding(COLLECT);
     }
 }
--- trunk/tagalog/tagalog-script/src/test/java/org/codehaus/tagalog/script/testsuite/tags/TestSuiteTagLibrary.java
+++ trunk/tagalog/tagalog-script/src/test/java/org/codehaus/tagalog/script/testsuite/tags/TestSuiteTagLibrary.java
@@ -1,24 +1,32 @@
 /*
- * $Id: TestSuiteTagLibrary.java,v 1.1 2005-04-05 17:17:27 mhw Exp $
+ * $Id: TestSuiteTagLibrary.java,v 1.2 2005-04-07 15:56:15 mhw Exp $
  */
 
 package org.codehaus.tagalog.script.testsuite.tags;
 
+import org.codehaus.tagalog.TagBinding;
 import org.codehaus.tagalog.script.tags.ScriptTagLibrary;
 
 /**
  * Tag library for describing {@link Script} unit tests in XML.
  *
  * @author <a href="mailto:mhw@kremvax.net">Mark Wilkinson</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class TestSuiteTagLibrary extends ScriptTagLibrary {
     public static final String NS_URI
         = "tagalog:script-test-suite";
 
+    public static final TagBinding TEST_SUITE = new TagBinding("test-suite",
+                                                               TestSuiteTag.class);
+    public static final TagBinding TEST = new TagBinding("test",
+                                                         TestTag.class);
+    public static final TagBinding EXPECTED = new TagBinding("expected",
+                                                             ExpectedTag.class);
+
     public TestSuiteTagLibrary() {
-        registerTag("test-suite", TestSuiteTag.class);
-        registerTag("test", TestTag.class);
-        registerTag("expected", ExpectedTag.class);
+        registerTagBinding(TEST_SUITE);
+        registerTagBinding(TEST);
+        registerTagBinding(EXPECTED);
     }
 }
