2da2b43a24e5d1ad8cd5c1fcb9e8d17781c92127
mhw <mhw@ea12b545-ab72-0410-9408-e459790c8658>
2005-04-07 15:51:33 +0000

    Update to use TagBinding objects. Move knowledge about Person objects from AttributeTag to PersonTag.

--- trunk/tagalog/tagalog-acceptance/src/main/java/org/codehaus/tagalog/acceptance/people/PersonTag.java
+++ trunk/tagalog/tagalog-acceptance/src/main/java/org/codehaus/tagalog/acceptance/people/PersonTag.java
@@ -1,18 +1,20 @@
 /*
- * $Id: PersonTag.java,v 1.5 2004-11-02 12:10:43 mhw Exp $
+ * $Id: PersonTag.java,v 1.6 2005-04-07 15:51:33 mhw Exp $
  */
 
 package org.codehaus.tagalog.acceptance.people;
 
 import org.codehaus.tagalog.AbstractTag;
 import org.codehaus.tagalog.Attributes;
+import org.codehaus.tagalog.TagBinding;
 import org.codehaus.tagalog.TagException;
+import org.codehaus.tagalog.TagalogParseException;
 
 /**
  * PersonTag
  *
  * @author <a href="mailto:mhw@kremvax.net">Mark Wilkinson</a>
- * @version $Revision: 1.5 $
+ * @version $Revision: 1.6 $
  */
 public final class PersonTag extends AbstractTag {
     Person person;
@@ -22,6 +24,15 @@ public final class PersonTag extends AbstractTag {
     {
         person = new Person();
         person.setUserId(requireAttribute(attributes, elementName, "user-id"));
+    }
+
+    public void child(TagBinding childType, Object child)
+        throws TagException, TagalogParseException
+    {
+        if (childType == PeopleTagLibrary.FIRST_NAME)
+            person.setFirstName((String) child);
+        else if (childType == PeopleTagLibrary.LAST_NAME)
+            person.setLastName((String) child);
     }
 
     public Object end(String elementName) {
