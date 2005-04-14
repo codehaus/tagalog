581a9794b85aea688e8dc025dd2bd3bbdae28585
mhw <mhw@ea12b545-ab72-0410-9408-e459790c8658>
2005-04-14 09:27:53 +0000

    Remove the parseErrors method; throw ParseFailedException instead.

--- tagalog/tagalog-jdbc/src/test/java/org/codehaus/tagalog/jdbc/CatalogTest.java
+++ tagalog/tagalog-jdbc/src/test/java/org/codehaus/tagalog/jdbc/CatalogTest.java
@@ -1,5 +1,5 @@
 /*
- * $Id: CatalogTest.java,v 1.3 2004-12-17 13:09:43 mhw Exp $
+ * $Id: CatalogTest.java,v 1.4 2005-04-14 09:27:53 mhw Exp $
  */
 
 package org.codehaus.tagalog.jdbc;
@@ -7,13 +7,16 @@ package org.codehaus.tagalog.jdbc;
 import java.net.URL;
 
 import org.codehaus.plexus.PlexusTestCase;
+
+import org.codehaus.tagalog.ParseError;
+import org.codehaus.tagalog.ParseFailedException;
 import org.codehaus.tagalog.TagalogParseException;
 
 /**
  * Basic tests for {@link Catalog}: construction and parsing.
  *
  * @author <a href="mailto:mhw@kremvax.net">Mark Wilkinson</a>
- * @version $Revision: 1.3 $
+ * @version $Revision: 1.4 $
  */
 public class CatalogTest extends PlexusTestCase {
     private URL url;
@@ -34,8 +37,14 @@ public class CatalogTest extends PlexusTestCase {
         }
 
         url = resource("CatalogTest-unknown-tag.xml");
-        catalog = new Catalog(getContainer(), url);
-        assertNotNull(catalog.parseErrors());
+        catalog = new Catalog(getContainer());
+        try {
+            catalog.parse(url);
+            fail("no parse errors");
+        } catch (ParseFailedException e) {
+            ParseError[] errors = e.getParseErrors();
+            assertEquals(1, errors.length);
+        }
         try {
             catalog.run("foo");
             fail("foo existed");
@@ -45,6 +54,14 @@ public class CatalogTest extends PlexusTestCase {
 
         url = resource("CatalogTest-good.xml");
         catalog = new Catalog(getContainer(), url);
-        assertEquals(0, catalog.parseErrors().length);
+
+        url = resource("CatalogTest-invalid.xml");
+        try {
+            catalog = new Catalog(getContainer(), url);
+            fail("no parse errors");
+        } catch (ParseFailedException e) {
+            ParseError[] errors = e.getParseErrors();
+            assertEquals(1, errors.length);
+        }
     }
 }
