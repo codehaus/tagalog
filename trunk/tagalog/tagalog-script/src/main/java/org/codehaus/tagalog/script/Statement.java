e66109dd0533431ff82a3c924f50f849a7f7b391
mhw <mhw@ea12b545-ab72-0410-9408-e459790c8658>
2005-04-19 16:23:31 +0000

    Add a null statement constant.

--- trunk/tagalog/tagalog-script/src/main/java/org/codehaus/tagalog/script/Statement.java
+++ trunk/tagalog/tagalog-script/src/main/java/org/codehaus/tagalog/script/Statement.java
@@ -1,5 +1,5 @@
 /*
- * $Id: Statement.java,v 1.1 2004-03-02 21:45:29 mhw Exp $
+ * $Id: Statement.java,v 1.2 2005-04-19 16:23:31 mhw Exp $
  */
 
 package org.codehaus.tagalog.script;
@@ -10,10 +10,15 @@ import java.util.Map;
  * Basic unit of execution in a {@link Script}.
  *
  * @author Mark H. Wilkinson
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public interface Statement {
     Object[] EMPTY_ARRAY = new Statement[0];
+
+    /**
+     * A statement that does nothing when evaluated.
+     */
+    Statement NULL = new NullStatement();
 
     /**
      * Execute a statement.
