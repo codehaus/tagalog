4a798c29e2a6f1e5d6fd7da494ff163e1c93baa7
mhw <mhw@ea12b545-ab72-0410-9408-e459790c8658>
2005-04-19 20:53:02 +0000

    Allow tests against reported errors, in addition to output from normal execution.

--- tagalog/tagalog-script/src/test/java/org/codehaus/tagalog/script/testsuite/TestSuite.java
+++ tagalog/tagalog-script/src/test/java/org/codehaus/tagalog/script/testsuite/TestSuite.java
@@ -1,5 +1,5 @@
 /*
- * $Id: TestSuite.java,v 1.1 2005-04-05 17:15:51 mhw Exp $
+ * $Id: TestSuite.java,v 1.2 2005-04-19 20:53:02 mhw Exp $
  */
 
 package org.codehaus.tagalog.script.testsuite;
@@ -12,13 +12,25 @@ import java.util.List;
  * A collection of script unit tests.
  *
  * @author <a href="mailto:mhw@kremvax.net">Mark Wilkinson</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public final class TestSuite {
     private List tests = new LinkedList();
 
     public void addTest(Test test) {
         tests.add(test);
+    }
+
+    public int caughtParseErrors() {
+        int count = 0;
+
+        Iterator iter = tests.iterator();
+
+        while (iter.hasNext()) {
+            Test test = (Test) iter.next();
+            count += test.caughtParseErrors();
+        }
+        return count;
     }
 
     public void run() throws Exception {
