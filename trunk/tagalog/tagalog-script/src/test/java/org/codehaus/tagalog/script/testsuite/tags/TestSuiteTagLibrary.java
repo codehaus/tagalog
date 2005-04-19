03580d6c586621545fbc3323d2fd12525179a10f
mhw <mhw@ea12b545-ab72-0410-9408-e459790c8658>
2005-04-19 17:22:29 +0000

    Use StringTag.

--- trunk/tagalog/tagalog-script/src/test/java/org/codehaus/tagalog/script/testsuite/tags/TestSuiteTagLibrary.java
+++ trunk/tagalog/tagalog-script/src/test/java/org/codehaus/tagalog/script/testsuite/tags/TestSuiteTagLibrary.java
@@ -1,17 +1,19 @@
 /*
- * $Id: TestSuiteTagLibrary.java,v 1.2 2005-04-07 15:56:15 mhw Exp $
+ * $Id: TestSuiteTagLibrary.java,v 1.3 2005-04-19 17:22:29 mhw Exp $
  */
 
 package org.codehaus.tagalog.script.testsuite.tags;
 
 import org.codehaus.tagalog.TagBinding;
+import org.codehaus.tagalog.script.Script;
 import org.codehaus.tagalog.script.tags.ScriptTagLibrary;
+import org.codehaus.tagalog.tags.StringTag;
 
 /**
  * Tag library for describing {@link Script} unit tests in XML.
  *
  * @author <a href="mailto:mhw@kremvax.net">Mark Wilkinson</a>
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
 public class TestSuiteTagLibrary extends ScriptTagLibrary {
     public static final String NS_URI
@@ -22,7 +24,7 @@ public class TestSuiteTagLibrary extends ScriptTagLibrary {
     public static final TagBinding TEST = new TagBinding("test",
                                                          TestTag.class);
     public static final TagBinding EXPECTED = new TagBinding("expected",
-                                                             ExpectedTag.class);
+                                                             StringTag.class);
 
     public TestSuiteTagLibrary() {
         registerTagBinding(TEST_SUITE);
