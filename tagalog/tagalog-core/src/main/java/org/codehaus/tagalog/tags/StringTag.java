b468acf16ddbbb47d37c0922653d33d9457561df
mhw <mhw@ea12b545-ab72-0410-9408-e459790c8658>
2005-04-26 14:29:57 +0000

    getName -> getElementName

--- tagalog/tagalog-core/src/main/java/org/codehaus/tagalog/tags/StringTag.java
+++ tagalog/tagalog-core/src/main/java/org/codehaus/tagalog/tags/StringTag.java
@@ -1,5 +1,5 @@
 /*
- * $Id: StringTag.java,v 1.1 2005-04-13 13:53:24 mhw Exp $
+ * $Id: StringTag.java,v 1.2 2005-04-26 14:29:57 mhw Exp $
  */
 
 package org.codehaus.tagalog.tags;
@@ -13,7 +13,7 @@ import org.codehaus.tagalog.TagalogParseException;
  * markup.
  *
  * @author <a href="mailto:mhw@kremvax.net">Mark Wilkinson</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public final class StringTag extends AbstractStringTag {
     public void child(TagBinding childType, Object child)
@@ -21,7 +21,7 @@ public final class StringTag extends AbstractStringTag {
     {
         TagBinding type = getTagBinding();
 
-        throw new TagException("<" + type.getElementName() + ">"
+        throw new TagException("<" + type.getName() + ">"
                                + " cannot contain markup");
     }
 }
