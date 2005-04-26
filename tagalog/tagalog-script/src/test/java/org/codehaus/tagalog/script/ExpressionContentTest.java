aa21ce27bf02ecf07d8662561b4fad559db91b03
mhw <mhw@ea12b545-ab72-0410-9408-e459790c8658>
2005-04-26 15:35:56 +0000

    Add test for expression language switching.

--- tagalog/tagalog-script/src/test/java/org/codehaus/tagalog/script/ExpressionContentTest.java
+++ tagalog/tagalog-script/src/test/java/org/codehaus/tagalog/script/ExpressionContentTest.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ExpressionContentTest.java,v 1.2 2005-04-14 14:08:40 mhw Exp $
+ * $Id: ExpressionContentTest.java,v 1.3 2005-04-26 15:35:56 mhw Exp $
  */
 
 package org.codehaus.tagalog.script;
@@ -13,13 +13,14 @@ import junit.framework.TestCase;
 import org.codehaus.tagalog.ParserConfiguration;
 import org.codehaus.tagalog.TagalogParser;
 import org.codehaus.tagalog.sax.TagalogSAXParserFactory;
+import org.codehaus.tagalog.script.tags.PITagLibrary;
 import org.codehaus.tagalog.script.tags.ScriptTagLibrary;
 
 /**
  * ExpressionContentTest
  *
  * @author <a href="mailto:mhw@kremvax.net">Mark Wilkinson</a>
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
 public class ExpressionContentTest extends TestCase {
     private ParserConfiguration configuration;
@@ -30,6 +31,7 @@ public class ExpressionContentTest extends TestCase {
         configuration = new ParserConfiguration();
         configuration.addTagLibrary(ScriptTagLibrary.NS_URI,
                                     new ScriptTagLibrary());
+        configuration.setProcessingInstructionTagLibrary(new PITagLibrary());
         factory = new TagalogSAXParserFactory(configuration);
     }
 
@@ -50,5 +52,26 @@ public class ExpressionContentTest extends TestCase {
 
         StringWriter out = (StringWriter) context.get("tagalog.out");
         assertEquals("Hello, Mark!", out.toString().trim());
+    }
+
+    public void testExpressionLanguageSwitching() throws Exception {
+        InputStream in;
+        TagalogParser parser;
+        Map context;
+
+        in = ScriptTest.class.getResourceAsStream("ExpressionLanguageSwitchingTest.xml");
+        parser = factory.createParser(in);
+        Script script = (Script) parser.parse();
+        assertEquals(0, parser.parseErrors().length);
+
+        context = new java.util.HashMap();
+        context.put("name", "Mark");
+        context.put("$", "ognl wouldn't like it");
+        context.put("tagalog.out", new StringWriter());
+        script.execute(context);
+
+        StringWriter out = (StringWriter) context.get("tagalog.out");
+        assertEquals("Hello, Mark (4 characters)!\nognl wouldn't like it",
+                     out.toString().trim());
     }
 }
