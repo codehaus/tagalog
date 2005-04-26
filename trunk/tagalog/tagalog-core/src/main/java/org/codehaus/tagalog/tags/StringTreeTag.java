9243ffc00f72aac004b61c9604524b2825ef0e96
mhw <mhw@ea12b545-ab72-0410-9408-e459790c8658>
2005-04-26 14:29:57 +0000

    getName -> getElementName

--- trunk/tagalog/tagalog-core/src/main/java/org/codehaus/tagalog/tags/StringTreeTag.java
+++ trunk/tagalog/tagalog-core/src/main/java/org/codehaus/tagalog/tags/StringTreeTag.java
@@ -1,5 +1,5 @@
 /*
- * $Id: StringTreeTag.java,v 1.1 2005-04-13 13:53:23 mhw Exp $
+ * $Id: StringTreeTag.java,v 1.2 2005-04-26 14:29:57 mhw Exp $
  */
 
 package org.codehaus.tagalog.tags;
@@ -13,7 +13,7 @@ import org.codehaus.tagalog.TagalogParseException;
  * elements.
  *
  * @author <a href="mailto:mhw@kremvax.net">Mark Wilkinson</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public final class StringTreeTag extends AbstractStringTag {
     public void child(TagBinding childType, Object child)
@@ -24,8 +24,8 @@ public final class StringTreeTag extends AbstractStringTag {
         else {
             TagBinding type = getTagBinding();
 
-            throw new TagException("child <" + childType.getElementName() + ">"
-                                   + " of <" + type.getElementName() + ">"
+            throw new TagException("child <" + childType.getName() + ">"
+                                   + " of <" + type.getName() + ">"
                                    + " returned " + child.getClass().getName()
                                    + " instead of String");
         }
