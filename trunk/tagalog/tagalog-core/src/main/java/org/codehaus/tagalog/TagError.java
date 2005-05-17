77efb1dbf87551e1326d7a95c8e95218b2d52edd
krisb <krisb@ea12b545-ab72-0410-9408-e459790c8658>
2005-05-17 13:22:46 +0000

    TagError introduced to cater for parsing errors in the tag handling.  Comments in TagalogParseException moved to TagError.

---
+++ trunk/tagalog/tagalog-core/src/main/java/org/codehaus/tagalog/TagError.java
@@ -0,0 +1,53 @@
+/*
+ * $Id: TagError.java,v 1.1 2005-05-17 13:22:46 krisb Exp $
+ */
+package org.codehaus.tagalog;
+
+
+/**
+ * <code>TagError</code> can be thrown by implementations of the 
+ * {@link Tag} interface if they encounter a condition that makes it 
+ * impossible to continue processing.<br>
+ * However, this style of processing is discouraged: in the majority of
+ * cases it is preferrable for <code>Tag</code>s to throw
+ * {@link TagException} as this allows parsing to continue and a better
+ * error report to be prepared for the user.
+ * <p>
+ * Cause is handled internally by this class to support pre J2SE 1.4.
+ * 
+ * @author <a href="mailto:krisb@codehaus.org">Kristopher Brown</a>
+ * @version $Revision: 1.1 $ $Date: 2005-05-17 13:22:46 $
+ */
+public class TagError extends Error {
+
+    private final Throwable cause;
+    
+    /**
+     * Creates a new instance of TagError with the supplied message.
+     * @param message the message
+     */
+    public TagError(String message) {
+        super(message);
+        this.cause = null;
+    }
+    
+    /**
+     * Creates a new instance of TagError with the supplied message and cause.
+     * @param message the message
+     * @param cause the cause
+     */
+    public TagError(String message, Throwable cause) {
+        super(message);
+        this.cause = cause;
+    }
+
+    /**
+     * Returns the cause of this TagError.
+     * @return the cause of this TagError
+     * @see java.lang.Throwable#getCause()
+     */
+    public final Throwable getCause() {
+        return cause;
+    }
+    
+}
--- trunk/tagalog/tagalog-core/src/main/java/org/codehaus/tagalog/TagalogParseException.java
+++ trunk/tagalog/tagalog-core/src/main/java/org/codehaus/tagalog/TagalogParseException.java
@@ -1,23 +1,17 @@
 /*
- * $Id: TagalogParseException.java,v 1.4 2005-05-17 10:57:51 mhw Exp $
+ * $Id: TagalogParseException.java,v 1.5 2005-05-17 13:22:46 krisb Exp $
  */
 
 package org.codehaus.tagalog;
 
 /**
  * <code>TagalogParseException</code> is used to wrap any exceptions
- * thrown by the underlying XML parser. It can also be thrown by
- * implementations of the {@link Tag} interface if they encounter a
- * condition that makes it impossible to continue processing.
- * However, this style of processing is discouraged: in the majority of
- * cases it is preferrable for <code>Tag</code>s to throw
- * {@link TagException} as this allows parsing to continue and a better
- * error report to be prepared for the user.
+ * thrown by the underlying XML parser.
  *
  * @see TagException
  *
  * @author <a href="mailto:mhw@kremvax.net">Mark Wilkinson</a>
- * @version $Revision: 1.4 $
+ * @version $Revision: 1.5 $
  */
 public class TagalogParseException extends Exception {
     public TagalogParseException(Throwable cause) {
