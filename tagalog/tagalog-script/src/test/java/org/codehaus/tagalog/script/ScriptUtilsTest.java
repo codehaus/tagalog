a2c9a927bfe2559bc430e2f126293bc765e4b6f6
krisb <krisb@ea12b545-ab72-0410-9408-e459790c8658>
2005-06-07 16:36:04 +0000

    replaced the implementation of escapeXml with a pre j2se 1.4 compliant implementation. the new implementation is also more performant, by a factor of around 10. the old implementation has been moved to the test to allow comparison still

--- tagalog/tagalog-script/src/test/java/org/codehaus/tagalog/script/ScriptUtilsTest.java
+++ tagalog/tagalog-script/src/test/java/org/codehaus/tagalog/script/ScriptUtilsTest.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ScriptUtilsTest.java,v 1.3 2005-04-26 15:30:37 mhw Exp $
+ * $Id: ScriptUtilsTest.java,v 1.4 2005-06-07 16:36:04 krisb Exp $
  */
 
 package org.codehaus.tagalog.script;
@@ -10,16 +10,72 @@ import junit.framework.TestCase;
  * Tests for the {@link ScriptUtils} class.
  *
  * @author <a href="mailto:mhw@kremvax.net">Mark Wilkinson</a>
- * @version $Revision: 1.3 $
+ * @version $Revision: 1.4 $
  */
 public class ScriptUtilsTest extends TestCase {
 
-    private void assertEscaped(String expected, String value) {
-        assertEquals(expected, ScriptUtils.escapeXml(value));
+    private static final int ITERATIONS = 10000;
+
+    public void testEscapeXmlNew() {
+        assertEquals("123&lt;45&gt;67",
+                ScriptUtils.escapeXml("123<45>67"));
+        assertEquals("&lt;&gt;&amp;&#039;&#034;",
+                ScriptUtils.escapeXml("<>&'\""));
+        assertNull(ScriptUtils.escapeXml(null));
     }
 
-    public void testEscapeXml() {
-        assertEscaped("123&lt;45&gt;67", "123<45>67");
-        assertEscaped("&lt;&gt;&amp;&#039;&#034;", "<>&'\"");
+    public void testSpeed() throws Exception {
+        long start, end, dur;
+        String test = "<>&'\"<>&'\"";
+        String expected = "&lt;&gt;&amp;&#039;&#034;&lt;&gt;&amp;&#039;&#034;";
+
+        start = System.currentTimeMillis();
+        for (int i = 0; i < ITERATIONS; i++) {
+            assertEquals(expected, escapeXmlOld(test));
+        }
+        end = System.currentTimeMillis();
+        dur = end - start;
+        System.out.println("old: " + dur + " ms");
+
+        start = System.currentTimeMillis();
+        for (int i = 0; i < ITERATIONS; i++) {
+            assertEquals(expected, ScriptUtils.escapeXml(test));
+        }
+        end = System.currentTimeMillis();
+        dur = end - start;
+        System.out.println("new: " + dur + " ms");
     }
+
+    // --- Old Implementation
+
+    private static String[][] xmlReplacements = {
+            { "&", "&amp;" },
+            { "<", "&lt;" },
+            { ">", "&gt;" },
+            { "'", "&#039;" },
+            { "\"", "&#034;" },
+        };
+
+    /**
+     * Replace characters that have significance in XML with their
+     * escaped equivalents, as per the JSTL specification.
+     *
+     * @param s String to be processed.
+     * @return Processed string.
+     */
+    private static String escapeXmlOld(String s) {
+        for (int i = 0; i < xmlReplacements.length; i++) {
+            s = s.replaceAll(xmlReplacements[i][0], xmlReplacements[i][1]);
+        }
+        return s;
+    }
+
+    public void testEscapeXmlOld() {
+        assertEquals("123&lt;45&gt;67",
+                escapeXmlOld("123<45>67"));
+        assertEquals("&lt;&gt;&amp;&#039;&#034;",
+                escapeXmlOld("<>&'\""));
+    }
+
+
 }
