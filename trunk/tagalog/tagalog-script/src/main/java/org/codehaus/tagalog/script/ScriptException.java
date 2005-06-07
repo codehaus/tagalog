263f108e4008cd43056712388cbe548ef675e4b9
krisb <krisb@ea12b545-ab72-0410-9408-e459790c8658>
2005-06-07 16:33:12 +0000

    pre j2se 1.4 cause support

--- trunk/tagalog/tagalog-script/src/main/java/org/codehaus/tagalog/script/ScriptException.java
+++ trunk/tagalog/tagalog-script/src/main/java/org/codehaus/tagalog/script/ScriptException.java
@@ -1,22 +1,27 @@
 /*
- * $Id: ScriptException.java,v 1.2 2005-04-26 16:42:08 mhw Exp $
+ * $Id: ScriptException.java,v 1.3 2005-06-07 16:33:12 krisb Exp $
  */
 
 package org.codehaus.tagalog.script;
 
 /**
  * Class of exception thrown by failures in script execution.
+ * <p>
+ * Cause is handled internally by this class to support pre J2SE 1.4.
  *
  * @author <a href="mailto:mhw@kremvax.net">Mark Wilkinson</a>
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
 public class ScriptException extends Exception {
+
+    private final Throwable cause;
 
     /**
      * @param message
      */
     public ScriptException(String message) {
         super(message);
+        this.cause = null;
     }
 
     /**
@@ -24,6 +29,16 @@ public class ScriptException extends Exception {
      * @param cause
      */
     public ScriptException(String message, Throwable cause) {
-        super(message, cause);
+        super(message);
+        this.cause = cause;
+    }
+
+    /**
+     * Returns the cause of this ScriptException.
+     * @return the cause of this ScriptException
+     * @see java.lang.Throwable#getCause()
+     */
+    public final Throwable getCause() {
+        return cause;
     }
 }
