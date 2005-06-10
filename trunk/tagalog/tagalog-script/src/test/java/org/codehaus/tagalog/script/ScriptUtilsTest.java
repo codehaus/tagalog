e8a032b9935527909c2752b34d4bdd39fff8ddab
krisb <krisb@ea12b545-ab72-0410-9408-e459790c8658>
2005-06-10 12:43:21 +0000

    tidied test up so that it just has the functional test now

--- trunk/tagalog/tagalog-script/src/test/java/org/codehaus/tagalog/script/ScriptUtilsTest.java
+++ trunk/tagalog/tagalog-script/src/test/java/org/codehaus/tagalog/script/ScriptUtilsTest.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ScriptUtilsTest.java,v 1.4 2005-06-07 16:36:04 krisb Exp $
+ * $Id: ScriptUtilsTest.java,v 1.5 2005-06-10 12:43:21 krisb Exp $
  */
 
 package org.codehaus.tagalog.script;
@@ -10,72 +10,18 @@ import junit.framework.TestCase;
  * Tests for the {@link ScriptUtils} class.
  *
  * @author <a href="mailto:mhw@kremvax.net">Mark Wilkinson</a>
- * @version $Revision: 1.4 $
+ * @version $Revision: 1.5 $
  */
 public class ScriptUtilsTest extends TestCase {
 
     private static final int ITERATIONS = 10000;
 
-    public void testEscapeXmlNew() {
+    public void testEscapeXml() {
         assertEquals("123&lt;45&gt;67",
                 ScriptUtils.escapeXml("123<45>67"));
         assertEquals("&lt;&gt;&amp;&#039;&#034;",
                 ScriptUtils.escapeXml("<>&'\""));
         assertNull(ScriptUtils.escapeXml(null));
     }
-
-    public void testSpeed() throws Exception {
-        long start, end, dur;
-        String test = "<>&'\"<>&'\"";
-        String expected = "&lt;&gt;&amp;&#039;&#034;&lt;&gt;&amp;&#039;&#034;";
-
-        start = System.currentTimeMillis();
-        for (int i = 0; i < ITERATIONS; i++) {
-            assertEquals(expected, escapeXmlOld(test));
-        }
-        end = System.currentTimeMillis();
-        dur = end - start;
-        System.out.println("old: " + dur + " ms");
-
-        start = System.currentTimeMillis();
-        for (int i = 0; i < ITERATIONS; i++) {
-            assertEquals(expected, ScriptUtils.escapeXml(test));
-        }
-        end = System.currentTimeMillis();
-        dur = end - start;
-        System.out.println("new: " + dur + " ms");
-    }
-
-    // --- Old Implementation
-
-    private static String[][] xmlReplacements = {
-            { "&", "&amp;" },
-            { "<", "&lt;" },
-            { ">", "&gt;" },
-            { "'", "&#039;" },
-            { "\"", "&#034;" },
-        };
-
-    /**
-     * Replace characters that have significance in XML with their
-     * escaped equivalents, as per the JSTL specification.
-     *
-     * @param s String to be processed.
-     * @return Processed string.
-     */
-    private static String escapeXmlOld(String s) {
-        for (int i = 0; i < xmlReplacements.length; i++) {
-            s = s.replaceAll(xmlReplacements[i][0], xmlReplacements[i][1]);
-        }
-        return s;
-    }
-
-    public void testEscapeXmlOld() {
-        assertEquals("123&lt;45&gt;67",
-                escapeXmlOld("123<45>67"));
-        assertEquals("&lt;&gt;&amp;&#039;&#034;",
-                escapeXmlOld("<>&'\""));
-    }
-
 
 }
